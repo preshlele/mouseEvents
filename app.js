eb441afbcec455f91c37f7dbefeb364a84288aaa
preshlele <71156004+preshlele@users.noreply.github.com>
2022-01-11 22:25:07 +0800

    Create app.js

---
+++ app.js
@@ -0,0 +1,37 @@
+// These variables store the boxes on the side
+let itemOne = document.getElementById('list-item-one');
+let itemTwo = document.getElementById('list-item-two');
+let itemThree = document.getElementById('list-item-three');
+let itemFour = document.getElementById('list-item-four');
+let itemFive = document.getElementById('list-item-five');
+let resetButton = document.getElementById('reset-button');
+
+// This function programs the "Reset" button to return the boxes to their default styles
+let reset = function() {
+  itemOne.style.width = ''
+  itemTwo .style.backgroundColor = ''
+  itemThree.innerHTML = 'The mouse must leave the box to change the text'
+  itemFive.style.display = "none"
+};
+resetButton.onclick = reset;
+
+// Write your code here
+let increaseWidth = function() {
+  itemOne.style.width = '420px';
+}
+itemOne.addEventListener('mouseover', increaseWidth);
+
+let changeBackground = function(){
+  itemTwo.style.backgroundColor = 'white';
+}
+itemTwo.addEventListener('mouseup',changeBackground);
+
+let changeText = function(){
+  itemThree.innerHTML = 'The mouse has left the element';
+}
+itemThree.addEventListener('mouseout', changeText);
+
+let showItem = function(){
+  itemFive.style.display = 'block';
+}
+itemFour.addEventListener('mousedown', showItem);
